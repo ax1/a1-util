7bc5e33a47433a9021be63dca0b6059be5c9f031
Angel Rego <angelrego@gmail.com>
2019-02-23 00:08:13 +0100

    util: execute() function

---
+++ src/util.ts
@@ -0,0 +1,16 @@
+import { promisify } from 'util'
+import { exec as node_exec } from 'child_process'
+const exec = promisify(node_exec)
+
+/**
+ * Generic call to an external process (ie: calling an executable file).
+ * Use with CAUTION. Check or sanitize the input (otherwise someone could perform rogue commands by adding data to the expected input string ).
+ * If stderr but exit was 0, the response is treated as Error
+ * @param {Promise<string>} command The instruction typed in the same way as typed in a terminal window. Examples: "ls -la | grep node" or "cat file.txt"  
+ */
+export async function execute(command: string): Promise<string> {
+  const { stdout, stderr } = await exec(command)
+  if (stderr.toString()) throw new Error(stderr.toString())
+  return stdout.toString()
+}
+
