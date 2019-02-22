ae1dbfa5857e9d8ae116c73b839774d127fb3126
Angel Rego <angelrego@gmail.com>
2019-02-23 00:52:24 +0100

    util: add colorized log() methods

--- src/util.ts
+++ src/util.ts
@@ -14,3 +14,10 @@ export async function execute(command: string): Promise<string> {
   return stdout.toString()
 }
 
+export function logOK(message?: any): void {
+  console.log('\x1b[32m%s\x1b[0m', message)
+}
+
+export function logError(message?: any): void {
+  console.log('\x1b[31m%s\x1b[0m', message)
+}
