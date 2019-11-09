29e40bd0558b00a11306c87713b1fbaf4a2dc034
Angel Rego <angelrego@gmail.com>
2019-11-09 15:04:17 +0100

    test: more tests

--- test/test.js
+++ test/test.js
@@ -2,8 +2,19 @@ const assert = require('assert')
 const util = require('../lib/util')
 
 async function test() {
+  await testExecute()
+  await testSleep()
+}
+
+async function testExecute() {
   const text = await util.execute('node -v')
   assert.ok(text.length > 0)
+}
+
+async function testSleep() {
+  console.log('1')
+  await util.sleep(1000)
+  console.log('2')
 }
 
 test().catch(err => util.logError(err))
